7ad566dac1e45d372c4c6bc24f36aef1d4ef5d99
Chris Zichko <chriszichkocoding@gmail.com>
2021-12-15 01:31:46 -0500

    Added comments to load on frontend, made Reply schema in Comment model and made the replies a subdocument to Comments and the ability to create/delete and show total comments/replies to a pizza

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,55 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const ReplySchema = new Schema(
+    {
+        replyBody: {
+            type: String,
+            default: () => new Types.ObjectId()
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
-});
+);
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+// get total ccount of replies
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+})
 
 const Comment = model('Comment', CommentSchema);
 
